6bf2b63dc9318454c057e34d0dccb78b8da33825
Chakradhar-Malage <chessp526@gmail.com>
2026-02-22 16:20:21 +0530

    Initialized backend in springboot framework

---
+++ src/main/java/com/Chakradhar/YesAuction/YesAuctionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Chakradhar.YesAuction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesAuctionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesAuctionBackendApplication.class, args);
+	}
+
+}
