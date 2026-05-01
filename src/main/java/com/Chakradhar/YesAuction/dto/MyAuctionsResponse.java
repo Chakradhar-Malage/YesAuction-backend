653d707433938dec9ec877678dba56a1764c3aee
Chakradhar-Malage <chessp526@gmail.com>
2026-05-01 23:58:17 +0530

    Changed auction response for my auctions

--- src/main/java/com/Chakradhar/YesAuction/dto/MyAuctionsResponse.java
+++ src/main/java/com/Chakradhar/YesAuction/dto/MyAuctionsResponse.java
@@ -5,13 +5,16 @@ import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class MyAuctionsResponse {
 	private Long id;
 	private String title;
 	private String description;
+	private String imageUrl;
 	private BigDecimal currentPrice;
 	private LocalDateTime endTime;
 	private String status;
